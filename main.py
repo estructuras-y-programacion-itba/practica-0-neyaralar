f9f4d1448a44784a1d220d3fc117eeef4a991a36
Nicolas Eyaralar <neyaralar@itba.edu.ar>
2026-03-05 17:23:09 -0300

    agrego funcion tirar dados

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # Tu implementacion va aqui
+import random
+
 def hola_mundo():
     return "hola_mundo"
 
 
 def main():
     # Aqui ejecutas tus soluciones
-    print(hola_mundo())
+    print(tirar_dados(5))
+
+def tirar_dados(cantidad):
+    tirada = []
+    for i in range (cantidad):
+        dado = random.randint(1, 6)
+        tirada.append(dado)
+    return tirada
+
 
 
 # No cambiar a partir de aqui
